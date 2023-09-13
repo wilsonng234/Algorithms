9bee9344143f74a3643ee963aae922f173dcd7b1
Wilson Ng <wilsonng234@gmail.com>
2023-09-13 21:54:12 +0800

    Test `InsertionSort`

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
-void InsertionSort(std::vector<int> &arr)
+namespace insertion_sort
 {
-    for (int i = 1; i < arr.size(); i++)
+    void InsertionSort(std::vector<int> &arr)
     {
-        int j = i - 1;
-        while (j >= 0 && arr[j] > arr[j + 1])
+        for (int i = 1; i < arr.size(); i++)
         {
-            std::swap(arr[j], arr[j + 1]);
-            j--;
+            int j = i - 1;
+            while (j >= 0 && arr[j] > arr[j + 1])
+            {
+                std::swap(arr[j], arr[j + 1]);
+                j--;
+            }
         }
     }
-}
 
-int main()
-{
-    std::vector<int> arr = {5, -2, 4, -6, 1, 3};
-    InsertionSort(arr);
+    void Test()
+    {
+        std::vector<int> arr = {5, -2, 4, -6, 1, 3};
+        InsertionSort(arr);
 
-    for (int i = 0; i < arr.size(); i++)
-        std::cout << arr[i] << " ";
+        assert(arr == std::vector<int>({-6, -2, 1, 3, 4, 5}));
+        std::cout << "All tests passed" << std::endl;
+    }
 }
