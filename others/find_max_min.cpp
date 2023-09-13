05e1507ad3482d4a5b3c308a1261afed28af74d6
Wilson Ng <wilsonng234@gmail.com>
2023-09-13 21:52:37 +0800

    Test `FindMaxMin`

--- others/find_max_min.cpp
+++ others/find_max_min.cpp
@@ -1,57 +1,62 @@
 #include <iostream>
+#include <tuple>
 #include <vector>
+#include <cassert>
 
-// Time complexity: O(n) : 3*(n-1)/2
-std::tuple<int, int> FindMaxMin(const std::vector<int> &array)
+namespace find_max_min
 {
-    if (array.empty())
-        throw std::invalid_argument("array is empty");
-
-    int i = 1;
-    int max = array[0];
-    int min = array[0];
-
-    while (i < array.size())
+    // Time complexity: O(n) : 3*(n-1)/2
+    std::tuple<int, int> FindMaxMin(const std::vector<int> &array)
     {
-        if (array[i] > array[i + 1])
+        if (array.empty())
+            throw std::invalid_argument("array is empty");
+
+        int i = 1;
+        int max = array[0];
+        int min = array[0];
+
+        while (i < array.size())
+        {
+            if (array[i] > array[i + 1])
+            {
+                if (array[i] > max)
+                    max = array[i];
+                if (array[i + 1] < min)
+                    min = array[i + 1];
+            }
+            else
+            {
+                if (array[i + 1] > max)
+                    max = array[i + 1];
+                if (array[i] < min)
+                    min = array[i];
+            }
+
+            i += 2;
+        }
+
+        if (i % 2)
         {
             if (array[i] > max)
                 max = array[i];
-            if (array[i + 1] < min)
-                min = array[i + 1];
-        }
-        else
-        {
-            if (array[i + 1] > max)
-                max = array[i + 1];
-            if (array[i] < min)
+            else if (array[i] < min)
                 min = array[i];
         }
 
-        i += 2;
+        return std::make_tuple(max, min);
     }
 
-    if (i % 2)
+    void Test()
     {
-        if (array[i] > max)
-            max = array[i];
-        else if (array[i] < min)
-            min = array[i];
+        int max, min;
+        std::vector<int> array = {11, -2, -3, 0, 1, -7, -26};
+        std::tie(max, min) = FindMaxMin(array);
+        assert(max == 11 && min == -26);
+
+        array.push_back(100);
+        std::tie(max, min) = FindMaxMin(array);
+        assert(max == 100 && min == -26);
+
+        std::cout << "All tests passed" << std::endl;
     }
-
-    return std::make_tuple(max, min);
-}
-
-int main()
-{
-    int max, min;
-    std::vector<int> array = {11, -2, -3, 0, 1, -7, -26};
-    std::tie(max, min) = FindMaxMin(array);
-    std::cout << max << " " << min << std::endl;
-
-    array.push_back(100);
-    std::tie(max, min) = FindMaxMin(array);
-    std::cout << max << " " << min << std::endl;
-
-    return 0;
 }
