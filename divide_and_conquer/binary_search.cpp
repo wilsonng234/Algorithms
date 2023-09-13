26a4c09c276bfe9d31a0f1f7eb4e81199ef3a32a
Wilson Ng <wilsonng234@gmail.com>
2023-09-13 21:50:20 +0800

    Test `BinarySearch`

--- divide_and_conquer/binary_search.cpp
+++ divide_and_conquer/binary_search.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
-int BinarySearch(const std::vector<int> &sorted_array, int target, int left, int right);
-
-int BinarySearch(const std::vector<int> &sorted_array, int target)
+namespace binary_search
 {
-    int left = 0;
-    int right = sorted_array.size() - 1;
+    int BinarySearch(const std::vector<int> &sorted_array, int target, int left, int right);
 
-    return BinarySearch(sorted_array, target, left, right);
-}
+    int BinarySearch(const std::vector<int> &sorted_array, int target)
+    {
+        int left = 0;
+        int right = sorted_array.size() - 1;
 
-int BinarySearch(const std::vector<int> &sorted_array, int target, int left, int right)
-{
-    if (left > right)
-        return -1;
+        return BinarySearch(sorted_array, target, left, right);
+    }
 
-    int mid = left + (right - left) / 2;
-    if (sorted_array[mid] == target)
-        return mid;
-    else if (sorted_array[mid] < target)
-        return BinarySearch(sorted_array, target, mid + 1, right);
-    else
-        return BinarySearch(sorted_array, target, left, mid - 1);
-}
+    int BinarySearch(const std::vector<int> &sorted_array, int target, int left, int right)
+    {
+        if (left > right)
+            return -1;
 
-int main()
-{
-    const std::vector<int> sorted_array = {-6, -2, 1, 3, 4, 5};
-    std::cout << BinarySearch(sorted_array, -6) << std::endl;
-    std::cout << BinarySearch(sorted_array, 1) << std::endl;
-    std::cout << BinarySearch(sorted_array, 5) << std::endl;
-    std::cout << BinarySearch(sorted_array, 0) << std::endl;
+        int mid = left + (right - left) / 2;
+        if (sorted_array[mid] == target)
+            return mid;
+        else if (sorted_array[mid] < target)
+            return BinarySearch(sorted_array, target, mid + 1, right);
+        else
+            return BinarySearch(sorted_array, target, left, mid - 1);
+    }
 
-    return 0;
+    void Test()
+    {
+        const std::vector<int> sorted_array = {-6, -2, 1, 3, 4, 5};
+        assert(BinarySearch(sorted_array, -6) == 0);
+        assert(BinarySearch(sorted_array, -2) == 1);
+        assert(BinarySearch(sorted_array, 1) == 2);
+        assert(BinarySearch(sorted_array, 3) == 3);
+        assert(BinarySearch(sorted_array, 4) == 4);
+        assert(BinarySearch(sorted_array, 5) == 5);
+        assert(BinarySearch(sorted_array, 0) == -1);
+        assert(BinarySearch(sorted_array, 6) == -1);
+
+        std::cout << "All tests passed" << std::endl;
+    }
 }
