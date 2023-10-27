b90ffc4f91f1d374a4e67e1c7c2db2bd6ea1ba3a
Wilson Ng <wilsonng234@gmail.com>
2023-10-27 15:32:27 +0800

    Create `WeightedGraph` class

--- graph/graph.py
+++ graph/graph.py
@@ -13,3 +13,19 @@ class Graph:
         self.vertices.add(u)
         self.vertices.add(v)
         self.edges[u].add(v)
+
+
+class WeightedGraph:
+    def __init__(self, edges=[]):
+        self.vertices = set()
+        self.edges = defaultdict(set)
+        self.weights = defaultdict(int)
+
+        for edge in edges:
+            self.addEdge(edge[0], edge[1], edge[2])
+
+    def addEdge(self, u, v, weight):
+        self.vertices.add(u)
+        self.vertices.add(v)
+        self.edges[u].add(v)
+        self.weights[(u, v)] = weight
